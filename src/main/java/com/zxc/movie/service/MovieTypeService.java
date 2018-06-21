a0c44160c3c0c7e54658fcb8be1306fdc1283e8e
while (true) <1748893247@qq.com>
2018-06-21 22:38:23 +0800

    比较小的类不提供给外界

--- src/main/java/com/zxc/movie/service/MovieTypeService.java
+++ src/main/java/com/zxc/movie/service/MovieTypeService.java
@@ -5,7 +5,9 @@ import com.zxc.movie.common.BaseService;
 
 /**
  * Created by 星驰太帅了 on 2018/06/03.
+ * 比较小的接口不提供给外界了，直接在  MovieService 添加对应的方法提供给外界
  * 电影类型操作类
  */
+@Deprecated
 public interface MovieTypeService extends BaseService<MovieType>{
 }
